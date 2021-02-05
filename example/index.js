4b2bb4712bac7ffa6f38a38ef6b6e9c69abdd022
Matthew <matthew.rebehn@gmail.com>
2021-02-04 21:31:57 -0500

    stripped out a bunch of stuff I did not need | allowed to parse without fill attribute

--- example/index.js
+++ example/index.js
@@ -1,31 +1,33 @@
-const parse = require("../lib");
+const parse = require('../lib');
 
 var svg = `
-        <svg width="722" height="112" class="js-calendar-graph-svg">
-            <g transform="translate(10, 20)" data-hydro-click="{&quot;event_type&quot;:&quot;user_profile.click&quot;,&quot;payload&quot;:{&quot;profile_user_id&quot;:2864371,&quot;target&quot;:&quot;CONTRIBUTION_CALENDAR_SQUARE&quot;,&quot;user_id&quot;:null,&quot;originating_url&quot;:&quot;https://github.com/users/IonicaBizau/contributions&quot;}}" data-hydro-click-hmac="c29bb84527b62dafc7ab4208ed2db21ea4195839d541da829d109a8d172bee42">
-                <g transform="translate(0, 0)">
-                <rect class="day" width="10" height="10" x="14" y="0" fill="var(--color-calendar-graph-day-bg)" data-count="0" data-date="2019-11-03"/>
-                <rect class="day" width="10" height="10" x="14" y="13" fill="var(--color-calendar-graph-day-bg)" data-count="0" data-date="2019-11-04"/>
-                <rect class="day" width="10" height="10" x="14" y="26" fill="var(--color-calendar-graph-day-bg)" data-count="0" data-date="2019-11-05"/>
-                <rect class="day" width="10" height="10" x="14" y="39" fill="var(--color-calendar-graph-day-bg)" data-count="0" data-date="2019-11-06"/>
-                <rect class="day" width="10" height="10" x="14" y="52" fill="var(--color-calendar-graph-day-bg)" data-count="0" data-date="2019-11-07"/>
-                <rect class="day" width="10" height="10" x="14" y="65" fill="var(--color-calendar-graph-day-bg)" data-count="0" data-date="2019-11-08"/>
-                <rect class="day" width="10" height="10" x="14" y="78" fill="var(--color-calendar-graph-day-bg)" data-count="0" data-date="2019-11-09"/>
-                </g>
-                <g transform="translate(14, 0)">
-                    <rect class="day" width="10" height="10" x="13" y="0" fill="var(--color-calendar-graph-day-bg)" data-count="0" data-date="2019-11-10"/>
-                    <rect class="day" width="10" height="10" x="13" y="13" fill="var(--color-calendar-graph-day-L1-bg)" data-count="4" data-date="2019-11-11"/>
-                    <rect class="day" width="10" height="10" x="13" y="26" fill="var(--color-calendar-graph-day-bg)" data-count="0" data-date="2019-11-12"/>
-                    <rect class="day" width="10" height="10" x="13" y="39" fill="var(--color-calendar-graph-day-L1-bg)" data-count="1" data-date="2019-11-13"/>
-                    <rect class="day" width="10" height="10" x="13" y="52" fill="var(--color-calendar-graph-day-bg)" data-count="0" data-date="2019-11-14"/>
-                    <rect class="day" width="10" height="10" x="13" y="65" fill="var(--color-calendar-graph-day-bg)" data-count="0" data-date="2019-11-15"/>
-                    <rect class="day" width="10" height="10" x="13" y="78" fill="var(--color-calendar-graph-day-bg)" data-count="0" data-date="2019-11-16"/>
-                </g>
+        <svg width="828" height="128" class="js-calendar-graph-svg">
+					<g transform="translate(10, 20)"
+						data-hydro-click="{&quot;event_type&quot;:&quot;user_profile.click&quot;,&quot;payload&quot;:{&quot;profile_user_id&quot;:24511162,&quot;target&quot;:&quot;CONTRIBUTION_CALENDAR_SQUARE&quot;,&quot;user_id&quot;:null,&quot;originating_url&quot;:&quot;https://github.com/users/Cipher-Coder/contributions&quot;}}"
+						data-hydro-click-hmac="a936b5f9afb917cc3e38789f909b25b84bb1ca8fbb08e7a6b19644970c6c3fef">
+						<g transform="translate(0, 0)">
+							<rect width="11" height="11" x="16" y="0" fill="var(--color-calendar-graph-day-bg)" class="day" data-count="2" data-date="2020-02-02" />
+							<rect width="11" height="11" x="16" y="15" fill="var(--color-calendar-graph-day-bg)" class="day" data-count="1" data-date="2020-02-03" />
+							<rect width="11" height="11" x="16" y="30" fill="var(--color-calendar-graph-day-bg)" class="day" data-count="1" data-date="2020-02-04" />
+							<rect width="11" height="11" x="16" y="45" fill="var(--color-calendar-graph-day-bg)" class="day" data-count="2" data-date="2020-02-05" />
+							<rect width="11" height="11" x="16" y="60" fill="var(--color-calendar-graph-day-bg)" class="day" data-count="0" data-date="2020-02-06" />
+							<rect width="11" height="11" x="16" y="75" fill="var(--color-calendar-graph-day-bg)" class="day" data-count="1" data-date="2020-02-07" />
+							<rect width="11" height="11" x="16" y="90" fill="var(--color-calendar-graph-day-bg)" class="day" data-count="2" data-date="2020-02-08" />
+						</g>
+						<g transform="translate(16, 0)">
+							<rect width="11" height="11" x="15" y="0" fill="var(--color-calendar-graph-day-bg)" class="day" data-count="0" data-date="2020-02-09" />
+							<rect width="11" height="11" x="15" y="15" fill="var(--color-calendar-graph-day-bg)" class="day" data-count="1" data-date="2020-02-10" />
+							<rect width="11" height="11" x="15" y="30" fill="var(--color-calendar-graph-day-bg)" class="day" data-count="1" data-date="2020-02-11" />
+							<rect width="11" height="11" x="15" y="45" fill="var(--color-calendar-graph-day-bg)" class="day" data-count="2" data-date="2020-02-12" />
+							<rect width="11" height="11" x="15" y="60" fill="var(--color-calendar-graph-day-bg)" class="day" data-count="1" data-date="2020-02-13" />
+							<rect width="11" height="11" x="15" y="75" fill="var(--color-calendar-graph-day-bg)" class="day" data-count="0" data-date="2020-02-14" />
+							<rect width="11" height="11" x="15" y="90" fill="var(--color-calendar-graph-day-bg)" class="day" data-count="0" data-date="2020-02-15" />
+						</g>
             </g>
         </svg>
 `;
 
-console.log(parse(svg))
+console.log(parse(svg));
 // {
 //     "last_year": 5,
 //     "longest_streak": 1,
