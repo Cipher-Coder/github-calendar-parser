21d3b5af3d6852b0a3a39cfd215a2d82ba397c65
Matthew <matthew.rebehn@gmail.com>
2021-02-09 10:49:42 -0500

    fixed streak

--- example/index.js
+++ example/index.js
@@ -6,22 +6,36 @@ var svg = `
 						data-hydro-click="{&quot;event_type&quot;:&quot;user_profile.click&quot;,&quot;payload&quot;:{&quot;profile_user_id&quot;:24511162,&quot;target&quot;:&quot;CONTRIBUTION_CALENDAR_SQUARE&quot;,&quot;user_id&quot;:null,&quot;originating_url&quot;:&quot;https://github.com/users/Cipher-Coder/contributions&quot;}}"
 						data-hydro-click-hmac="a936b5f9afb917cc3e38789f909b25b84bb1ca8fbb08e7a6b19644970c6c3fef">
 						<g transform="translate(0, 0)">
-							<rect width="11" height="11" x="16" y="0" fill="var(--color-calendar-graph-day-bg)" class="day" data-count="2" data-date="2020-02-02" />
-							<rect width="11" height="11" x="16" y="15" fill="var(--color-calendar-graph-day-bg)" class="day" data-count="1" data-date="2020-02-03" />
-							<rect width="11" height="11" x="16" y="30" fill="var(--color-calendar-graph-day-bg)" class="day" data-count="1" data-date="2020-02-04" />
-							<rect width="11" height="11" x="16" y="45" fill="var(--color-calendar-graph-day-bg)" class="day" data-count="2" data-date="2020-02-05" />
-							<rect width="11" height="11" x="16" y="60" fill="var(--color-calendar-graph-day-bg)" class="day" data-count="0" data-date="2020-02-06" />
-							<rect width="11" height="11" x="16" y="75" fill="var(--color-calendar-graph-day-bg)" class="day" data-count="1" data-date="2020-02-07" />
-							<rect width="11" height="11" x="16" y="90" fill="var(--color-calendar-graph-day-bg)" class="day" data-count="2" data-date="2020-02-08" />
+							<rect width="11" height="11" x="16" y="0" class="ContributionCalendar-day" rx="2" ry="2"
+								data-count="0" data-date="2020-02-09" data-level="0"></rect>
+							<rect width="11" height="11" x="16" y="15" class="ContributionCalendar-day" rx="2" ry="2"
+								data-count="1" data-date="2020-02-10" data-level="1"></rect>
+							<rect width="11" height="11" x="16" y="30" class="ContributionCalendar-day" rx="2" ry="2"
+								data-count="1" data-date="2020-02-11" data-level="1"></rect>
+							<rect width="11" height="11" x="16" y="45" class="ContributionCalendar-day" rx="2" ry="2"
+								data-count="2" data-date="2020-02-12" data-level="1"></rect>
+							<rect width="11" height="11" x="16" y="60" class="ContributionCalendar-day" rx="2" ry="2"
+								data-count="1" data-date="2020-02-13" data-level="1"></rect>
+							<rect width="11" height="11" x="16" y="75" class="ContributionCalendar-day" rx="2" ry="2"
+								data-count="0" data-date="2020-02-14" data-level="0"></rect>
+							<rect width="11" height="11" x="16" y="90" class="ContributionCalendar-day" rx="2" ry="2"
+								data-count="0" data-date="2020-02-15" data-level="0"></rect>
 						</g>
 						<g transform="translate(16, 0)">
-							<rect width="11" height="11" x="15" y="0" fill="var(--color-calendar-graph-day-bg)" class="day" data-count="0" data-date="2020-02-09" />
-							<rect width="11" height="11" x="15" y="15" fill="var(--color-calendar-graph-day-bg)" class="day" data-count="1" data-date="2020-02-10" />
-							<rect width="11" height="11" x="15" y="30" fill="var(--color-calendar-graph-day-bg)" class="day" data-count="1" data-date="2020-02-11" />
-							<rect width="11" height="11" x="15" y="45" fill="var(--color-calendar-graph-day-bg)" class="day" data-count="2" data-date="2020-02-12" />
-							<rect width="11" height="11" x="15" y="60" fill="var(--color-calendar-graph-day-bg)" class="day" data-count="1" data-date="2020-02-13" />
-							<rect width="11" height="11" x="15" y="75" fill="var(--color-calendar-graph-day-bg)" class="day" data-count="0" data-date="2020-02-14" />
-							<rect width="11" height="11" x="15" y="90" fill="var(--color-calendar-graph-day-bg)" class="day" data-count="0" data-date="2020-02-15" />
+							<rect width="11" height="11" x="15" y="0" class="ContributionCalendar-day" rx="2" ry="2"
+								data-count="1" data-date="2020-02-16" data-level="1"></rect>
+							<rect width="11" height="11" x="15" y="15" class="ContributionCalendar-day" rx="2" ry="2"
+								data-count="0" data-date="2020-02-17" data-level="0"></rect>
+							<rect width="11" height="11" x="15" y="30" class="ContributionCalendar-day" rx="2" ry="2"
+								data-count="1" data-date="2020-02-18" data-level="1"></rect>
+							<rect width="11" height="11" x="15" y="45" class="ContributionCalendar-day" rx="2" ry="2"
+								data-count="0" data-date="2020-02-19" data-level="0"></rect>
+							<rect width="11" height="11" x="15" y="60" class="ContributionCalendar-day" rx="2" ry="2"
+								data-count="1" data-date="2020-02-20" data-level="1"></rect>
+							<rect width="11" height="11" x="15" y="75" class="ContributionCalendar-day" rx="2" ry="2"
+								data-count="0" data-date="2020-02-21" data-level="0"></rect>
+							<rect width="11" height="11" x="15" y="90" class="ContributionCalendar-day" rx="2" ry="2"
+								data-count="2" data-date="2020-02-22" data-level="1"></rect>
 						</g>
             </g>
         </svg>
